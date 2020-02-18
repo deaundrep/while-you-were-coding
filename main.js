9bf642cc234551e5b07c65b403945f78733433a8
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-02-18 08:43:27 -0500

    done

--- main.js
+++ main.js
@@ -9,7 +9,9 @@
 
 // ##### Challenges #####
 
-// 1.
+function makeChallengeBanner(n) {
+    console.log(`######## Challenge ${n} ########`)
+}
 //
 // Write a function that takes in a parameter and prints the string below, with the given paramter place of the $ symbol.
 // 
@@ -20,42 +22,59 @@
 // Even though we'll USUALLY be calling this function with a number indicating the challenge we're on, THIS TIME call it with the parameter 'Banner Function Works!'. You should see the below result:
 //
 // ###### Challenge Banner Function Works! #####
-
-
-// 2.
+makeChallengeBanner(1);
 //
 // Console out the numbers from 1-20, but only if they're divisible by 3.
+for (num =1 ; num<=20; num++) {
+    if (num%3===0)
+    console.log(num);
+}
+    num = num - 1
+i = 1;
 
 // Use `if` and a modulus operation.
-
-
-// 3.
+makeChallengeBanner(2)
+if(0) {
+    console.log("");
+} else {
+    console.log("");
+}
 //
 // Same problem as #2, but no `if`s, `mods`, or `but`s (and no, `but` is not a JavaScript tool). There may be more ways to solve this problem without `if` or `%` (there usually are in JavaScript!), but if you did it the way we did, you should run the loop 6 times with console log per loop iteration.
-
-
-
-// 4.
-//
+makeChallengeBanner(3)
 // Console out the numbers from 1-20. BACKWARDS.
-
-
-// 5.
+makeChallengeBanner(4)
+for (i=20; i >= 1; i--){
+    console.log(i);
+}
 //
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
+makeChallengeBanner(5)
+const str = "BoyhowdyamIgoodatthis!";
+const strArray = str.split('');
 
-
-// 6.
+strArray.map(s => console.log(s));
 //
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
+makeChallengeBanner(6)
+const str = "BoyhowdyamIgoodatthis!";
+const strArray = str.split('');
 
-
-// 7. 
+strArray.map(s => console.log(s));
 //
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
+makeChallengeBanner(7)
+let str = 'Am I the best?';
+let letterSeven = 0;
+while (letterSeven < str.length) {
+    console.log(str[letterSeven].toUpperCase());
+    letterSeven++;
+}
+
+
 
 
 // 8.
